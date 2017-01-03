72409dcfeb14f06132daf25388dce6856c774386
Dominique VASSARD <dominique.vassard@kwanko.com>
2017-01-03 14:49:18 +0100

    Make it work with Neo4j (3.1)

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from eve import Eve
+from eve_neo4j import Neo4j
 
 
-app = Eve()
+app = Eve(data=Neo4j)
 app.run(host='0.0.0.0', debug=True)
--- settings.py
+++ settings.py
@@ -1,1 +1,13 @@
+# flask-neo4j has an import problem
+# should be
+# import py2neo.ogm as ogm
+# of
+# from py2neo.ext import ogm
+GRAPH_DATABASE = 'http://localhost:7474/db/data/'
+GRAPH_USER = 'neo4j'
+GRAPH_PASSWORD = 'neo4j'
+
+# TODO: Override this as a default when using Neo4j as a data layer
+ITEM_URL = 'regex("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")'
+
 DOMAIN = {'people': {}}
