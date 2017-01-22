26e96011cd59f9f39497c091a975d90d8d848f08
Dominique VASSARD <dominique.vassard@gmail.com>
2017-01-22 11:13:40 +0100

    Fully working example with sawgger

--- api/run.py
+++ api/run.py
@@ -1,6 +1,46 @@
 from eve import Eve
 from eve_neo4j import Neo4j
+from eve_swagger import swagger, add_documentation
 
 
 app = Eve(data=Neo4j)
+
+app.register_blueprint(swagger)
+
+# required. See http://swagger.io/specification/#infoObject for details.
+app.config['SWAGGER_INFO'] = {
+    'title': 'test Eve with Swagger',
+    'version': '1.0',
+    'description': 'an API description',
+    'termsOfService': 'my terms of service',
+    # 'contact': {
+    #     'name': 'dominique',
+    #     'url': '---'
+    # },
+    # 'license': {
+    #     'name': 'BSD',
+    #     'url': 'mygithub',
+    # }
+}
+
+# optional. Will use flask.request.host if missing.
+# app.config['SWAGGER_HOST'] = 'localhost:8000'
+
+# optional. Add/Update elements in the documentation at run-time without deleting subtrees.
+# add_documentation({'paths': {'/status': {'get': {'parameters': [
+#     {
+#         'in': 'query',
+#         'name': 'foobar',
+#         'required': False,
+#         'description': 'special query parameter',
+#         'type': 'string'
+#     }]
+# }}}})
+
+
+@app.route('/test_out_eve')
+def tets_out_eve():
+    return 'Out of Eve test'
+
+
 app.run(host='0.0.0.0', debug=True)
--- api/settings.py
+++ api/settings.py
@@ -3,11 +3,58 @@
 # import py2neo.ogm as ogm
 # of
 # from py2neo.ext import ogm
-GRAPH_DATABASE = 'http://neo4j:7474/db/data/'
+GRAPH_DATABASE = 'http://localhost:7474/db/data/'
 GRAPH_USER = 'neo4j'
 GRAPH_PASSWORD = 'njpa_reil123'
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+# Swagger conf
+X_DOMAINS = ['http://localhost:5000',  # The domain where Swagger UI is running
+             'http://editor.swagger.io',
+             'http://petstore.swagger.io']
+X_HEADERS = ['Content-Type', 'If-Match']  # Needed for the "Try it out" buttons
 
 # TODO: Override this as a default when using Neo4j as a data layer
 ITEM_URL = 'regex("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")'
 
-DOMAIN = {'people': {}}
+user_schema = {
+    'firstName': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15
+    },
+    'lastName': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 30,
+    },
+    'login': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 12,
+        'required': True,
+        # 'unique': True
+    }
+}
+user = {
+    'schema': user_schema,
+    'resource_methods': ['GET', 'POST', 'DELETE'],
+
+    'datasource': {
+        'projection': {
+            'login': 0
+        }
+    },
+    'additional_lookup': {
+        'url': 'regex("[\w+]")',
+        'field': 'login'
+    }
+}
+
+DOMAIN = {
+    'user': user
+}
+
+# [{"login": "user1", "firstName": "John", "lastName": "DUFF"}, {"login": "user2"}, {"login": "user3", "firstName": "Richard", "lastName": "DASSAULT"}]
